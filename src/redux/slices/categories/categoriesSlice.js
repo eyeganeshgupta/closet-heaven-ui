e08d7863d17d8333fd67f73eb2a04085f68e05f5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-25 19:54:12 +0530

    fetch-all-categories

--- src/redux/slices/categories/categoriesSlice.js
+++ src/redux/slices/categories/categoriesSlice.js
@@ -51,3 +51,107 @@ export const createCategoryAction = createAsyncThunk(
     }
   }
 );
+
+// ! fetch all categories action
+export const fetchAllCategoriesAction = createAsyncThunk(
+  "category/fetch-all-categories",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // TODO: make the request
+      const { data } = await axios.get(`${baseURL}/categories`);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+// ! category slice
+const categorySlice = createSlice({
+  name: "category",
+  initialState,
+  extraReducers: (builder) => {
+    // * ----- handle create category lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(createCategoryAction.pending, (state) => {
+      state.loading = true;
+      state.categories = [];
+      state.category = {};
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? fulfilled
+    builder.addCase(createCategoryAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.categories = [];
+      state.category = action.payload;
+      state.isAdded = true;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? rejected
+    builder.addCase(createCategoryAction.rejected, (state, action) => {
+      state.loading = false;
+      state.categories = [];
+      state.category = {};
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = action.payload;
+    });
+
+    // * ----- handle fetch all categories lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(fetchAllCategoriesAction.pending, (state) => {
+      state.loading = true;
+      state.categories = [];
+      state.category = {};
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? fulfilled
+    builder.addCase(fetchAllCategoriesAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.categories = action.payload;
+      state.category = {};
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? rejected
+    builder.addCase(fetchAllCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.categories = [];
+      state.category = {};
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = action.payload;
+    });
+
+    // * ----- reset success action -----
+    builder.addCase(resetSuccessAction.pending, (state, action) => {
+      state.isAdded = false;
+    });
+
+    // * ----- reset error action -----
+    builder.addCase(resetErrorAction.pending, (state) => {
+      state.error = null;
+    });
+  },
+});
+
+// ! generate the reducer
+const categoryReducer = categorySlice.reducer;
+
+export default categoryReducer;
