ee78b155110ae05ac0abf71ae25d50a1bf7cd65b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-25 19:53:35 +0530

    create-category-action

--- src/redux/slices/categories/categoriesSlice.js
+++ src/redux/slices/categories/categoriesSlice.js
@@ -16,3 +16,38 @@ const initialState = {
   isDeleted: false,
   error: null,
 };
+
+// ! create category action
+export const createCategoryAction = createAsyncThunk(
+  "category/create",
+  async ({ name, file }, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // TODO 01: get the token for Authentication
+      const token = getState().users?.userAuth?.userInfo?.token;
+
+      // TODO 02: Pass the token for Authentication
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      // TODO 03: FormData
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("file", file);
+
+      // TODO 04: make the request
+      const { data } = await axios.post(
+        `${baseURL}/categories`,
+        formData,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
