a819ace64deb3372f04e9d877da555dcf522b6bc
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-25 19:49:15 +0530

    cart-slice

--- src/redux/slices/cart/cartSlice.js
+++ src/redux/slices/cart/cartSlice.js
@@ -72,3 +72,82 @@ export const removeOrderFromCartAction = createAsyncThunk(
     localStorage.setItem("cartItems", JSON.stringify(newItems));
   }
 );
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  extraReducers: (builder) => {
+    // * ----- handle add to cart lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(addOrderToCartAction.pending, (state) => {
+      state.loading = true;
+      state.cartItems = [];
+      state.error = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+    });
+
+    // ? fullfilled
+    builder.addCase(addOrderToCartAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.cartItems = action.payload;
+      state.error = null;
+      state.isAdded = true;
+      state.isUpdated = false;
+      state.isDeleted = false;
+    });
+
+    // ? rejected
+    builder.addCase(addOrderToCartAction.rejected, (state, action) => {
+      state.loading = false;
+      state.cartItems = null;
+      state.error = action.payload;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+    });
+
+    // * ----- get cart items from localStorage lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(getCartItemsFromLocalStorageAction.pending, (state) => {
+      state.loading = true;
+      state.cartItems = [];
+      state.error = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+    });
+
+    // ? fullfilled
+    builder.addCase(
+      getCartItemsFromLocalStorageAction.fulfilled,
+      (state, action) => {
+        state.loading = false;
+        state.cartItems = action.payload;
+        state.error = null;
+        state.isAdded = false;
+        state.isUpdated = false;
+        state.isDeleted = false;
+      }
+    );
+
+    // ? rejected
+    builder.addCase(
+      getCartItemsFromLocalStorageAction.rejected,
+      (state, action) => {
+        state.loading = false;
+        state.cartItems = null;
+        state.error = action.payload;
+        state.isAdded = false;
+        state.isUpdated = false;
+        state.isDeleted = false;
+      }
+    );
+  },
+});
+
+// * generate reducer
+const cartReducer = cartSlice.reducer;
+
+export default cartReducer;
