8b8e278e5e82aa285a0d4e8fabf91e8bad67704e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-26 19:17:01 +0530

    colors-reducer

--- src/redux/slices/colors/colorsSlice.js
+++ src/redux/slices/colors/colorsSlice.js
@@ -62,3 +62,93 @@ export const fetchAllColorsAction = createAsyncThunk(
     }
   }
 );
+
+// ! color slice
+const colorsSlice = createSlice({
+  name: "colors",
+  initialState,
+  extraReducers: (builder) => {
+    // * ----- handle create color lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(createColorAction.pending, (state) => {
+      state.loading = true;
+      state.colors = [];
+      state.color = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? fulfilled
+    builder.addCase(createColorAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.colors = [];
+      state.color = action.payload;
+      state.isAdded = true;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? rejected
+    builder.addCase(createColorAction.rejected, (state, action) => {
+      state.loading = false;
+      state.colors = [];
+      state.color = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = action.payload;
+    });
+
+    // * ----- handle fetch all colors lifecycle - pending, fulfilled, rejected -----
+    // ? pending
+    builder.addCase(fetchAllColorsAction.pending, (state) => {
+      state.loading = true;
+      state.colors = [];
+      state.color = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? fulfilled
+    builder.addCase(fetchAllColorsAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.colors = action.payload;
+      state.color = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = null;
+    });
+
+    // ? rejected
+    builder.addCase(fetchAllColorsAction.rejected, (state, action) => {
+      state.loading = false;
+      state.colors = [];
+      state.color = null;
+      state.isAdded = false;
+      state.isUpdated = false;
+      state.isDeleted = false;
+      state.error = action.payload;
+    });
+
+    // * ----- reset success action -----
+    builder.addCase(resetSuccessAction.pending, (state, action) => {
+      state.isAdded = false;
+    });
+
+    // * ----- reset error action -----
+    builder.addCase(resetErrorAction.pending, (state) => {
+      state.error = null;
+    });
+  },
+});
+
+// ! generate the reducer
+const colorsReducer = colorsSlice.reducer;
+
+export default colorsReducer;
