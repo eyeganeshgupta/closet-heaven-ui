e9596b55e696193ac12f8926758ecd1e17d5c969
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-26 19:18:31 +0530

    create-coupon-action

---
+++ src/redux/slices/coupons/couponsSlice.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import baseURL from "../../../utils/baseURL";
+import {
+  resetErrorAction,
+  resetSuccessAction,
+} from "../globalActions/globalActions";
+
+// ! initialState
+const initialState = {
+  loading: false,
+  coupons: [],
+  coupon: null,
+  isExists: false,
+  isAdded: false,
+  isUpdated: false,
+  isDeleted: false,
+  error: null,
+};
+
+// ! create coupon action
+export const createCouponAction = createAsyncThunk(
+  "coupon/create",
+  async (
+    { code, discount, startDate, endDate },
+    { rejectWithValue, getState, dispatch }
+  ) => {
+    try {
+      // TODO 01: get the token for Authentication
+      const token = getState().users?.userAuth?.userInfo?.token;
+
+      // TODO 02: Pass the token for Authentication
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // TODO 03: make the request
+      const { data } = await axios.post(
+        `${baseURL}/coupons`,
+        {
+          code,
+          discount,
+          startDate,
+          endDate,
+        },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
