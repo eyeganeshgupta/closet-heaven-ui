d90ac6adeb12ea67c10ff68d4f96bd08d37bdc06
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-27 17:48:59 +0530

    reset-success-action

--- src/redux/slices/globalActions/globalActions.js
+++ src/redux/slices/globalActions/globalActions.js
@@ -4,3 +4,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const resetErrorAction = createAsyncThunk("reset-error-action", () => {
   return {};
 });
+
+// * reset success action
+export const resetSuccessAction = createAsyncThunk(
+  "reset-success-action",
+  () => {
+    return {};
+  }
+);
