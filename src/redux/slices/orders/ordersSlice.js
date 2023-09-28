3b88b3847006c9db870415644a17fd91607fef7f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-28 18:46:07 +0530

    place-an-order-action

--- src/redux/slices/orders/ordersSlice.js
+++ src/redux/slices/orders/ordersSlice.js
@@ -16,3 +16,34 @@ const initialState = {
   isUpdated: false,
   error: null,
 };
+
+// ! place order action
+export const placeOrderAction = createAsyncThunk(
+  "order/place-order",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const { orderItems, shippingAddress, totalPrice } = payload;
+
+      // TODO 01: get the token for Authentication
+      const token = getState().users?.userAuth?.userInfo?.token;
+
+      // TODO 02: Pass the token for Authentication
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // TODO 03: make the request
+      const { data } = await axios.post(
+        `${baseURL}/orders`,
+        { orderItems, shippingAddress, totalPrice },
+        config
+      );
+
+      return window.open(data?.url);
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
