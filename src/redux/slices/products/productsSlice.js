c20fdaef20769f366ff54f98746e6b9d5e1483bc
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-29 20:21:08 +0530

    create-product-action

--- src/redux/slices/products/productsSlice.js
+++ src/redux/slices/products/productsSlice.js
@@ -16,3 +16,67 @@ const initialState = {
   isDeleted: false,
   error: null,
 };
+
+// ! create product action
+export const createProductAction = createAsyncThunk(
+  "product/create",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    console.log(payload);
+    try {
+      // TODO 00: destruct payload
+      const {
+        name,
+        description,
+        brand,
+        category,
+        sizes,
+        colors,
+        price,
+        totalQty,
+        files,
+      } = payload;
+
+      // TODO 01: get the token for Authentication
+      const token = getState().users?.userAuth?.userInfo?.token;
+
+      // TODO 02: Pass the token for Authentication
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      // TODO 03: Create the product with images
+      // * FormData
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("brand", brand);
+      formData.append("category", category);
+      sizes.forEach((size) => {
+        formData.append("sizes", size);
+      });
+      colors.forEach((color) => {
+        formData.append("colors", color);
+      });
+      formData.append("price", price);
+      formData.append("totalQty", totalQty);
+      files.forEach((file) => {
+        formData.append("files", file);
+      });
+
+      // TODO 04: make the request
+      const { data } = await axios.post(
+        `${baseURL}/products`,
+        formData,
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
