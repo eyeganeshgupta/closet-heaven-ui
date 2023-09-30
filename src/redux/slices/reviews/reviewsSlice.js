4308dd6a074a625b2f606158eb579c4b8066010c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-30 18:28:21 +0530

    create-review-action

--- src/redux/slices/reviews/reviewsSlice.js
+++ src/redux/slices/reviews/reviewsSlice.js
@@ -16,3 +16,34 @@ const initialState = {
   isDeleted: false,
   error: null,
 };
+
+// ! create review action
+export const createReviewAction = createAsyncThunk(
+  "review/create",
+  async ({ rating, message, id }, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // TODO 01: get the token for Authentication
+      const token = getState().users?.userAuth?.userInfo?.token;
+
+      // TODO 02: Pass the token for Authentication
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // TODO 03: make the request
+      const { data } = await axios.post(
+        `${baseURL}/reviews/${id}`,
+        {
+          rating,
+          message,
+        },
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
