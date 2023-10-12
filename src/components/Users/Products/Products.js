308ed30fa6320ae1dac08fbed2b13099dff9f238
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-12 10:42:50 +0530

    removing-add-to-cart-plus-icon

--- src/components/Users/Products/Products.js
+++ src/components/Users/Products/Products.js
@@ -64,28 +64,6 @@ const Products = ({ products }) => {
                       </p>
                     </a>
                   </Link>
-                  <a
-                    className="ml-auto mr-2 flex items-center justify-center w-12 h-12 bg-blue-300 hover:bg-blue-400 rounded-md"
-                    href="#"
-                  >
-                    <svg
-                      width={12}
-                      height={12}
-                      viewBox="0 0 12 12"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <rect x={5} width={2} height={12} fill="white" />
-                      <rect
-                        x={12}
-                        y={5}
-                        width={2}
-                        height={12}
-                        transform="rotate(90 12 5)"
-                        fill="white"
-                      />
-                    </svg>
-                  </a>
                 </div>
               </div>
             </div>
